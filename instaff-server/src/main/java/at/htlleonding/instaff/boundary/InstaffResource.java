a59af44d5691d2e6958c47d5819347d2662a326e
Philip Pfarrhofer <p.pfarrhofer@students.htl-leonding.ac.at>
2024-10-21 12:45:33 +0200

    classes added

--- instaff-server/src/main/java/at/htlleonding/instaff/boundary/InstaffResource.java
+++ instaff-server/src/main/java/at/htlleonding/instaff/boundary/InstaffResource.java
@@ -1,29 +1,43 @@
 package at.htlleonding.instaff.boundary;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import at.htlleonding.instaff.model.Company;
+import at.htlleonding.instaff.model.Manager;
+import at.htlleonding.instaff.repository.ManagerRespository;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import java.io.InputStream;
 import jakarta.ws.rs.core.Response;
 
 @Path("/")
 public class InstaffResource {
+    ManagerRespository managerRespository = new ManagerRespository();
 
     @GET
-    @Produces(MediaType.TEXT_HTML)
-    public Response getIndexPage() {
-        // Load the index.html file from the resources/web directory
-        InputStream indexStream = getClass().getResourceAsStream("/web/index.html");
-        if (indexStream == null) {
-            return Response.status(Response.Status.NOT_FOUND)
-                    .entity("index.html not found")
-                    .build();
-        }
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/company/{companyId}")
+    public Response getCompany(@PathParam("companyId") int companyId) {
+        Response.Status status = companyId < 0 ? Response.Status.NO_CONTENT : Response.Status.OK;
+        Company company = new Company(companyId, "Test Company");
+        return Response.status(status)
+                .entity(company).build();
+    }
 
-        // Return the file as an HTML response
-        return Response.ok(indexStream)
-                .type(MediaType.TEXT_HTML)
-                .build();
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/manager/{managerId}")
+    public Response getManager(@PathParam("managerId") int managerId) {
+        Manager manager = managerRespository.getManager(managerId);
+        Response.Status status = manager == null ? Response.Status.NO_CONTENT : Response.Status.OK;
+        return Response.status(status)
+                .entity(manager).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/createmanager/{managerName}/{password}/{companyId}")
+    public Response createManager(@PathParam("managerName") String managerName, @PathParam("password") String password, @PathParam("companyId") int companyId) {
+        managerRespository.addManager(new Manager(this.managerRespository.getNextId(), managerName, password, companyId));
+        return Response.status(Response.Status.CREATED)
+                .entity(this.managerRespository.getAllManagers()).build();
     }
 }
---
+++ instaff-server/src/main/java/at/htlleonding/instaff/model/Employee.java
@@ -0,0 +1,12 @@
+package at.htlleonding.instaff.model;
+
+public class Employee {
+    private int employeeId;
+    private String employeeName;
+    private String employeeEmail;
+    private String employeePassword;
+    private String employeePhoneNumber;
+    private String employeeBirthdate;
+    private String employeeSalary;
+    private Company company;
+}
