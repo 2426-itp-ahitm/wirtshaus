1c08c988c75c9b6f25bf6b62054a4cdaff309874
KlementTobias <t.klement@students.htl-leonding.ac.at>
2024-11-25 11:43:56 +0100

    working on filter api call

--- web/src/components/employee-filter-roles/employee-filter-roles-service.ts
+++ web/src/components/employee-filter-roles/employee-filter-roles-service.ts
@@ -1,13 +1,9 @@
 import { Employee } from "../../models/employee"
-
 const BASE_URL = "/api"
-const role_id = new URLSearchParams(window.location.search).get("role_id")
-
 export async function loadEmployeesFilteredByRole(role_name: string) { 
    console.log("###################################")
-   console.log("Role ID: "+role_id)  
    console.log("Role Name: "+role_name)
-   const response = await fetch(`${BASE_URL}/employees/role/1`)
+   const response = await fetch(`${BASE_URL}/employees/role/Koch`)
    const employees: Employee[] = await response.json()
    
    console.log(role_name);
