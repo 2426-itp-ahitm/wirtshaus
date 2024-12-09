30845298e2bdb837cd01b0e9fbe1f35626f7296b
KlementTobias <t.klement@students.htl-leonding.ac.at>
2024-12-09 15:23:10 +0100

    working #69

--- web/src/components/add-employee/add-employee-component.ts
+++ web/src/components/add-employee/add-employee-component.ts
@@ -47,62 +47,51 @@ class AddEmployeeComponent extends HTMLElement {
    
 
    tableTemplate(employees: Employee[]) {
-      const rows = employees.map(
-         (employee) => html`
-            <tr>
-               <td>${employee.firstname}</td>
-               <td>${employee.lastname}</td>
-               <td>${employee.email}</td>
-               <td>${employee.telephone}</td>
-               <td>${employee.birthdate}</td>
-               <td>${employee.company_name}</td>
-            </tr>
-         `
-      );
 
       return html`
             <h2>Add a Employee</h2>
+            <form>
+                <label for="first_name">First Name</label>
+                <input
+                type="text"
+                id="first_name"
+                name="first_name"
+                value=${this.inputValue}
+                />
 
-            <label for="first_name">First Name</label>
-            <input
-            type="text"
-            id="first_name"
-            name="first_name"
-            value=${this.inputValue}
-            />
+                <label for="last_name">Last Name</label>
+                <input
+                type="text"
+                id="last_name"
+                name="last_name"
+                value=${this.inputValue}
+                />
 
-            <label for="last_name">Last Name</label>
-            <input
-            type="text"
-            id="last_name"
-            name="last_name"
-            value=${this.inputValue}
-            />
+                <label for="email">Email</label>
+                <input
+                type="text"
+                id="email"
+                name="email"
+                value=${this.inputValue}
+                />
 
-            <label for="email">Email</label>
-            <input
-            type="text"
-            id="email"
-            name="email"
-            value=${this.inputValue}
-            />
+                <label for="telephone">Telephone</label>
+                <input
+                type="text"
+                id="telephone"
+                name="telephone"
+                value=${this.inputValue}
+                />
 
-            <label for="telephone">Telephone</label>
-            <input
-            type="text"
-            id="telephone"
-            name="telephone"
-            value=${this.inputValue}
-            />
+                <label for="birthdate">Birthdate</label>
+                <input
+                type="text"
+                id="birthdate"
+                name="birthdate"
+                value=${this.inputValue}
+                />
+            </form>
 
-            <label for="birthdate">Birthdate</label>
-            <input
-            type="text"
-            id="birthdate"
-            name="birthdate"
-            value=${this.inputValue}
-            />
-            
       `;
    }
 }
