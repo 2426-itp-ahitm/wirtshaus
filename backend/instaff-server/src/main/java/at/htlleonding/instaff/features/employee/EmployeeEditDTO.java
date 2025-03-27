74e77c4c4f03d8cce0a6b7ab5b9fb6891e75a3b5
Philip Pfarrhofer <philip.pfarrhofer@gmail.com>
2025-03-27 13:46:02 +0100

    fixed user update error

--- backend/instaff-server/src/main/java/at/htlleonding/instaff/features/employee/EmployeeEditDTO.java
+++ backend/instaff-server/src/main/java/at/htlleonding/instaff/features/employee/EmployeeEditDTO.java
@@ -1,12 +1,15 @@
 package at.htlleonding.instaff.features.employee;
 
+import javax.management.relation.Role;
 import java.time.LocalDate;
+import java.util.List;
 
 public record EmployeeEditDTO(
         String firstname,
         String lastname,
         String email,
         LocalDate birthdate,
-        String telephone
+        String telephone,
+        List<Role> roles
 ) {
 }
