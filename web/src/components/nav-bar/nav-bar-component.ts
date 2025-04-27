a875c8f9006f60565279f414ab85057fd4aa3eda
Alexander Hahn <alexanderhahn@MacBook-Pro-von-Alexander.local>
2025-04-27 16:48:22 +0200

    finished #137

--- web/src/components/nav-bar/nav-bar-component.ts
+++ web/src/components/nav-bar/nav-bar-component.ts
@@ -17,7 +17,6 @@ const template = () => html`
          <div class="navbar-start">
             <a class="navbar-item" href="#/employee-list">Employee List</a>
             <a class="navbar-item" href="#/shift-list">Shift List</a>
-            <a class="navbar-item" href="#/add-shift">Add Shift</a>
             <a class="navbar-item" href="#/edit-shift">Edit Shift</a>
          </div>
       </div>
