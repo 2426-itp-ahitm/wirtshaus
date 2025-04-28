ddc440cd1a0e761f0435f7ac79ce6d42e8219284
Alexander Hahn <alexanderhahn@MacBook-Pro-von-Alexander.local>
2025-04-28 10:51:07 +0200

    working #139

---
+++ iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Alexander Hahn on 28.04.25.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/iOSTests/iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTests.swift
+//  iOSTests
+//
+//  Created by Alexander Hahn on 28.04.25.
+//
+
+import Testing
+@testable import iOS
+
+struct iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
