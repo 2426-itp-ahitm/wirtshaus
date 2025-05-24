a5c610ccbf95f8da123a60c4b7e528fec914ddb0
KlementTobias <t.klement@students.htl-leonding.ac.at>
2025-05-24 15:02:16 +0200

    added fullcalendar.io

--- frontend/src/app/calendar/calendar.component.ts
+++ frontend/src/app/calendar/calendar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [CommonModule, RouterOutlet, FullCalendarModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
 export class CalendarComponent {
-
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin]
+  };
 }
