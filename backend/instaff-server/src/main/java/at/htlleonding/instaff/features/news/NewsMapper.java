132fe1370ee3723eb5a126151de2c3245e9c56d3
pfarrhoferphilip <p.pfarrhofer@students.htl-leonding.ac.at>
2025-11-28 10:12:33 +0100

    finished #214

--- backend/instaff-server/src/main/java/at/htlleonding/instaff/features/news/NewsMapper.java
+++ backend/instaff-server/src/main/java/at/htlleonding/instaff/features/news/NewsMapper.java
@@ -6,7 +6,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 public class NewsMapper {
 
     public NewsDTO toResource(final News news) {
-        return new NewsDTO(news.assignment.getEmployee().getFullName(), news.assignmentStatus,
+        return new NewsDTO(news.getId(), news.assignment.getEmployee().getFullName(), news.assignmentStatus,
                 news.assignment.getShift().getStartTime(), news.assignment.getShift().getId(), news.dateCreated);
     }
 }
